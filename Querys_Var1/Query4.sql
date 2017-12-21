e29548152d683be7c8dde22f09445a7f13c7b35c
daniil <daniil.ermolaevv@gmail.com>
2017-12-21 22:25:44 +0300

    Optimized query 4 to variant 1

--- Querys_Var1/Query4.sql
+++ Querys_Var1/Query4.sql
@@ -27,18 +27,18 @@ HAVING MAX(CountRentCar) > = (
 							 
 --this query faster then first
 ----------------------------------------------------------
-WITH CountRentCarsOffice_CTE (RecN, CountRentCar, Dohod, CountRentContract) 
+WITH CountRentCarsOffice_CTE (OffCode, City, Addr, RecN, CountRentCar, Dohod, CountRentContract) 
 AS	(
-		SELECT O.RecN, COUNT(DISTINCT C.CarN) AS CountRentCar, SUM((C.DailyPay*Con.PlanDays)+(C.DailyPay*Con.OverDays)+(Con.OverDays*Con.Fine)) as Dohod, COUNT(Con.ArN) as CountRentContract
+		SELECT O.OffCode, O.City, O.Addr, O.RecN, COUNT(DISTINCT C.CarN) AS CountRentCar, SUM((C.DailyPay*Con.PlanDays)+(C.DailyPay*Con.OverDays)+(Con.OverDays*Con.Fine)) as Dohod, COUNT(Con.ArN) as CountRentContract
 		FROM Office O
 		JOIN Contract Con ON O.RecN =  Con.GetRecN 
 		JOIN Car C ON Con.CarN = C.CarN
-		GROUP BY O.RecN
+		GROUP BY O.RecN, O.OffCode, O.City, O.Addr
 	)
 
-SELECT MAX(CountRentCar) CountRentCar, CCTE.RecN, Dohod, CCTE.CountRentContract
+SELECT MAX(CountRentCar) CountRentCar, CCTE.RecN, Dohod, CCTE.CountRentContract, OffCode, City, Addr 
 FROM CountRentCarsOffice_CTE CCTE
-GROUP BY CCTE.RecN, Dohod, CCTE.CountRentContract
+GROUP BY CCTE.RecN, Dohod, CCTE.CountRentContract, OffCode, City, Addr
 HAVING MAX(CountRentCar) > = (
 								SELECT MAX(CountRentCar)
 								FROM(	
